367eb2ad3f2e069a06da503fd65612cc9c115252
Jonas Karlsson <jonaskarlsson@fripost.org>
2016-07-31 04:52:50 +0200

    Remove unused code

--- keyboard_input.h
+++
@@ -1,9 +0,0 @@
-#ifndef __KEYBOARD_INPUT_H_
-#define __KEYBOARD_INPUT_H_
-
-#include "command.h"
-#include "mapping_key.h"
-
-void get_input_clavier(int nbmapping,struct mapping_key* keymap);
-
-#endif
--- player_input.h
+++ player_input.h
@@ -5,8 +5,6 @@
 #include "mapping_key.h"
 #include "mapping_joy.h"
 
-void get_input_clavier(int nbmapping,struct mapping_key* keymap);
-
 void get_control_input(int nbplayers, struct command* cmds, bool key_down[]);
 
 void get_key_input(struct command *cmd, bool key_down[]);
