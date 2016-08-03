e25117c33a923cfed3b4834e1bd32d1e542b4456
Jonas Karlsson <jonaskarlsson@fripost.org>
2016-08-03 19:39:42 +0200

    Format vaisseau_data

--- vaisseau_data.h
+++ vaisseau_data.h
@@ -11,84 +11,83 @@
 #define VAISSEAU_ANGLESTEP 5
 #define VAISSEAU_MAX_FUEL 1284
 #define VAISSEAU_SPEED_FUEL_DOWN 1
-#define VAISSEAU_SPEED_FUEL_UP 8 
+#define VAISSEAU_SPEED_FUEL_UP 8
 #define VAISSEAU_MAX_SHIELD_FORCE 214
 #define VAISSEAU_SPEED_SHIELD_FORCE_DOWN 2
 #define VAISSEAU_SPEED_SHIELD_FORCE_UP 2
 
 struct debris_data {
-    int   x, y;
-    double xposprecise,yposprecise;
-    double ax, ay;
-    double vx, vy;
-    double impultion;
-    int   angle;
-    bool  active;
+  int x, y;
+  double xposprecise, yposprecise;
+  double ax, ay;
+  double vx, vy;
+  double impultion;
+  int angle;
+  bool active;
 };
 
 struct tir_data {
-	int x, y;
-    double xposprecise, yposprecise;
-    double dx, dy;
-	bool free;
+  int x, y;
+  double xposprecise, yposprecise;
+  double dx, dy;
+  bool free;
 };
 
 struct vaisseau_data {
-	// special for each ship
-    double mass;
-    double thrust_max;
-	int anglestep;
+  // special for each ship
+  double mass;
+  double thrust_max;
+  int anglestep;
 
-    int fuel;
-    int shield_force;
+  int fuel;
+  int shield_force;
 
-	int max_fuel;
-	int speed_fuel_down;
-	int speed_fuel_up;
-    bool refueling;
-	int max_shield_force;
-	int speed_shield_force_down;
-	int speed_shield_force_up;
+  int max_fuel;
+  int speed_fuel_down;
+  int speed_fuel_up;
+  bool refueling;
+  int max_shield_force;
+  int speed_shield_force_down;
+  int speed_shield_force_up;
 
-    int explode_count;
-    bool explode;
-    int  option_type;
-	int option_expire_time;
+  int explode_count;
+  bool explode;
+  int option_type;
+  int option_expire_time;
 
-	int impactx,impacty;
-    double ax,ay;
-    double vx,vy;
-    double xposprecise,yposprecise;
-	int xpos,ypos;
-	int angle;
-    double thrust;
-	bool shield;
-    bool fire;
-    bool fire_delay;
-	bool landed;
-	bool rebound;
-    ALLEGRO_BITMAP   *sprite_buffer;   // Image du vaisseau
-    ALLEGRO_BITMAP	 *sprite_buffer_rota;
-	struct vaisseau_gfx * gfx;
-    struct tir_data tir[MAX_TIR];
-    struct tir_data backtir[MAX_TIR];
+  int impactx, impacty;
+  double ax, ay;
+  double vx, vy;
+  double xposprecise, yposprecise;
+  int xpos, ypos;
+  int angle;
+  double thrust;
+  bool shield;
+  bool fire;
+  bool fire_delay;
+  bool landed;
+  bool rebound;
+  ALLEGRO_BITMAP *sprite_buffer; // Image du vaisseau
+  ALLEGRO_BITMAP *sprite_buffer_rota;
+  struct vaisseau_gfx *gfx;
+  struct tir_data tir[MAX_TIR];
+  struct tir_data backtir[MAX_TIR];
 
-    struct debris_data debris[8];  // 8 debris
-  };
+  struct debris_data debris[8]; // 8 debris
+};
 
+int init_vaisseau_data(struct vaisseau_data *v, struct vaisseau_gfx *gfx,
+                       float mass, float thrust_max, int anglestep,
+                       int max_fuel, int speed_fuel_down, int speed_fuel_up,
+                       int max_shield_force, int speed_shield_force_down,
+                       int speed_shield_force_up);
 
+void clean_vaisseau_data(struct vaisseau_data *v);
 
-int init_vaisseau_data(struct vaisseau_data* v, struct vaisseau_gfx* gfx,
-                              float mass, float thrust_max, int anglestep,
-                              int max_fuel, int speed_fuel_down, int speed_fuel_up,
-                              int max_shield_force, int speed_shield_force_down, int speed_shield_force_up);
-
-void clean_vaisseau_data(struct vaisseau_data* v);
-
-void init_ship_pos_from_platforms(struct vaisseau_data* v, struct platform_data * plats);
+void init_ship_pos_from_platforms(struct vaisseau_data *v,
+                                  struct platform_data *plats);
 
 // to be called at each vbl!
 void fuel_shield_calcul(int nbvaisseau, struct vaisseau_data *v);
-
 
 #endif
