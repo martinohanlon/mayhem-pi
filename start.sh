96949faae784cdff77d9ed3f7dd5cb0b19b7c4b2
Martin O'Hanlon <martin@ohanlonweb.com>
2016-12-08 22:33:59 +0000

    updated start script

--- start.sh
+++ start.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
 #check there is enough gpu memory to start mayhem
-source /boot/config.txt
-if [[ !  -z  $gpu_mem  ]]; then
-    if (( $gpu_mem >= 96 )); then
-        ./mayhem2-pi
-    else
-        echo "Mayhem 2 needs at least 96MB of memory allocated to the GPU"
-        echo "Use sudo raspi-config (Advanced Options > Memory Split) to change"
-    fi
+vcgencmd get_mem gpu > gpu_mem
+source gpu_mem
+#strip last char from 64M
+gpu=${gpu%?}
+if (( $gpu >= 96 )); then
+    ./mayhem2-pi
 else
     echo "Mayhem 2 needs at least 96MB of memory allocated to the GPU"
     echo "Use sudo raspi-config (Advanced Options > Memory Split) to change"
 fi
-
+rm gpu_mem
